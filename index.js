750e1298c35d5c4604bf389a230fec8be939fa71
Jim McGee <jmcgee86@gmail.com>
2017-12-30 13:05:40 +0000

    v1 completed

---
+++ index.js
@@ -0,0 +1,10 @@
+var todos = ["item 1", "item 2", "item 3"];
+
+console.log("My todos:", todos);
+
+todos.push("item 4", "item 5");
+
+todos[0] = "item 1 updated";
+
+todos.splice(0,1);
+todos.splice(3, 1);
