e00a3a928c5c566994591aca261ff5b89f52a35a
Jim McGee <jmcgee86@gmail.com>
2017-12-30 13:48:30 +0000

    version 2 complete

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var todos = ["item 1", "item 2", "item 3"];
-
+//function to display todos
 function displayTodos(){
     console.log("My todos:", todos);
 }
-
+//function to add todos to todo array
 function addTodo(todo){
 	todos.push(todo);
 	displayTodos();
 }
-
+//function to change todos in todo array
 function changeTodo(position, newValue){
 	todos[position] = newValue;
 	displayTodos();
 }
-
+//function to delete todos from todo array
 function deleteTodo(position){
 	todos.splice(position,1);
 	displayTodos();
