575303d927cc80e846102c4d1c1ea85520c47088
Jim McGee <jmcgee86@gmail.com>
2017-12-30 13:21:34 +0000

    function to display todos

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var todos = ["item 1", "item 2", "item 3"];
 
-console.log("My todos:", todos);
-
-todos.push("item 4", "item 5");
-
-todos[0] = "item 1 updated";
-
-todos.splice(0,1);
-todos.splice(3, 1);
+function displayTodos(){
+    console.log("My todos:", todos);
+}
