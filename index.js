8e3d7e157226dd97dcee2105dd119b483e910958
Jim McGee <jmcgee86@gmail.com>
2017-12-31 20:12:47 +0000

    has a displayTodos method

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 
 var todoList = {
-	todos: ['item 1', 'item 2', 'item 3'],	
+	todos: ['item 1', 'item 2', 'item 3'],
+	displayTodos: function (){
+		console.log("My todos", this.todos);
+	}
 };
 
-//function to display todos
-function displayTodos(){
-    console.log("My todos:", todos);
-}
 //function to add todos to todo array
 function addTodo(todo){
 	todos.push(todo);
