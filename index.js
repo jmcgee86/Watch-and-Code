23c6ebe65509f6da7482e1e1547331f6a5d57cda
Jim McGee <jmcgee86@gmail.com>
2017-12-31 20:16:15 +0000

    has an addTodo method

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var todoList = {
 	todos: ['item 1', 'item 2', 'item 3'],
 	displayTodos: function (){
 		console.log("My todos", this.todos);
+	},
+	addTodo: function(todo){
+		this.todos.push(todo);
+		this.displayTodos();
 	}
 };
-
-//function to add todos to todo array
-function addTodo(todo){
-	todos.push(todo);
-	displayTodos();
-}
+/*
 //function to change todos in todo array
 function changeTodo(position, newValue){
 	todos[position] = newValue;
@@ -21,3 +20,4 @@ function deleteTodo(position){
 	todos.splice(position,1);
 	displayTodos();
 }
+*/
