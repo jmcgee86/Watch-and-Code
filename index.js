fbffe5c7597bd642a0e5ed471efc10a6c0276ced
Jim McGee <jmcgee86@gmail.com>
2018-01-04 15:18:20 +0000

    toggle all button working

--- index.js
+++ index.js
@@ -60,8 +60,14 @@ var todoList = {
 	}
 };
 
+
 var displayTodosButton = document.getElementById("displayTodosButton");
+var toggleAllButton = document.getElementById("toggleAllButton");	
 
 displayTodosButton.addEventListener('click', function(){
 	todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function(){
+	todoList.toggleAll();
 });
