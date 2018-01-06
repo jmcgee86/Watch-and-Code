f2be2f58ecbae649639afa7eac857073884139cb
Jim McGee <jmcgee86@gmail.com>
2018-01-06 20:28:42 +0000

    each todo item has a delete button

--- index.js
+++ index.js
@@ -89,7 +89,14 @@ var view = {
 			}
 			
 			todoLi.textContent = todoTextWithCompletion;
+			todoLi.appendChild(this.createDeleteButton())
 			todosUl.appendChild(todoLi);
 		}
+	},
+	createDeleteButton: function(){
+		var deleteButton = document.createElement('button');
+		deleteButton.textContent = 'Delete';
+		deleteButton.className = 'deleteButton';
+		return deleteButton;
 	}
 };
