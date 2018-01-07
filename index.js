83082df3e3caa9baa0b824ed8d5a0418230a6cd8
Jim McGee <jmcgee86@gmail.com>
2018-01-07 14:23:41 +0000

    displayTodos uses forEach

--- index.js
+++ index.js
@@ -71,22 +71,23 @@ var view = {
 	displayTodos: function(){
 	var todosUl = document.querySelector('ul');
 	todosUl.innerHTML = "";
-		for (var i = 0; i<todoList.todos.length; i++){
-			var todoLi = document.createElement('li');
-			var todo = todoList.todos[i];
-			var todoTextWithCompletion = "";
-			
-			if (todo.completed === true){
-				todoTextWithCompletion = '(x)' + todo.todoText;
+
+	todoList.todos.forEach(function(todo, position){
+		var todoLi = document.createElement('li');
+		var todoTextWithCompletion = "";
+		
+		if (todo.completed === true){
+			todoTextWithCompletion = '(x)' + todo.todoText;
 			} else {
-				todoTextWithCompletion = '( )' + todo.todoText;
+			todoTextWithCompletion = '( )' + todo.todoText;
 			}
 			
-			todoLi.id = i;
+			todoLi.id = position;
 			todoLi.textContent = todoTextWithCompletion;
 			todoLi.appendChild(this.createDeleteButton())
-			todosUl.appendChild(todoLi);
-		}
+			todosUl.appendChild(todoLi);	
+	}, this);
+	
 	},
 	createDeleteButton: function(){
 		var deleteButton = document.createElement('button');
